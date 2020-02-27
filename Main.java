68ea98415279001d9f166dab77481efba1cc9cc5
Eryk Kowalewicz <eryk.kowalewicz@outlook.com>
2020-02-27 13:58:50 +0000

    Added Code

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
 public class Main {
     // Main method
+    public static void main(String[] args) {
+        Person perObj1 = new Person(); 
+
+        perObj1.setuserName("Erk3357");
+        System.out.println(perObj1.getuserName());
+
+        perObj1.setfirstName("Eryk");
+        System.out.println(perObj1.getfirstName());
+
+        perObj1.setmiddleNames("GDPR");
+        System.out.println(perObj1.getmiddleNames());
+    }
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,37 @@
-class Person {
+public class Person {
     // Instance Variables
-    
+    private String firstName;
+    private String middleNames;
+    private String userName;
+
     // Methods
+
+    // Getters
+    public String getfirstName() {
+        return firstName;
+
+    }
+
+    public String getmiddleNames() {
+        return middleNames;
+
+    }
+
+    public String getuserName() {
+        return userName;
+
+    }
+
+    // Setters
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setmiddleNames(String middleNames) {
+        this.middleNames = middleNames;
+    }
+
+    public void setuserName(String userName) {
+        this.userName = userName;
+    }
 }
